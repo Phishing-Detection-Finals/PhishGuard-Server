fc55c18a01ca5911cb532bc4fca8acd1517e3748
gilazani <gilazani1@gmail.com>
2024-05-30 14:23:33 +0300

    user setting controller modified to use ExceptionsHandler

--- PhishGuard/src/controllers/user_setting_controller.py
+++ PhishGuard/src/controllers/user_setting_controller.py
@@ -12,6 +12,7 @@ from ..exceptions.username_not_valid_exception import UsernameNotValidException
 from ..exceptions.missing_required_fields_exception import MissingRequiredFieldsException
 from email_validator import EmailNotValidError
 from ..validator import Validator
+from ..utils.exceptions_handler import ExceptionsHandler
 
 
 class UserSettingController:
@@ -29,104 +30,63 @@ class UserSettingController:
         self.blueprint.route('', methods=['GET'])(self.get_user_details)
 
     @jwt_required()
+    @ExceptionsHandler.handle_exceptions({
+        UserNotExistsException: HTTPStatus.NOT_FOUND
+    })
     def delete_user(self):
-        try:
-            return jsonify(UserService().delete_user(identity=get_jwt_identity())), HTTPStatus.OK
-
-        except UserNotExistsException as e:
-            return jsonify({"error": str(e)}), HTTPStatus.NOT_FOUND
-
-        except Exception as e:
-            return jsonify({"error": str(e)}), HTTPStatus.INTERNAL_SERVER_ERROR
+        return jsonify(UserService().delete_user(identity=get_jwt_identity())), HTTPStatus.OK
 
     @jwt_required()
+    @ExceptionsHandler.handle_exceptions({
+        UserNotExistsException: HTTPStatus.NOT_FOUND
+    })
     def get_user_details(self):
-        try:
-            user_email = get_jwt_identity()
-            user_dict = UserService().get_user_details_by_email(email=user_email)
-            return jsonify(user_dict), HTTPStatus.OK
-
-        except UserNotExistsException as e:
-            return jsonify({"error": str(e)}), HTTPStatus.NOT_FOUND
-
-        except Exception as e:
-            return jsonify({"error": str(e)}), HTTPStatus.INTERNAL_SERVER_ERROR
+        user_email = get_jwt_identity()
+        user_dict = UserService().get_user_details_by_email(email=user_email)
+        return jsonify(user_dict), HTTPStatus.OK
 
     @jwt_required()
+    @ExceptionsHandler.handle_exceptions({
+        MissingRequiredFieldsException: HTTPStatus.BAD_REQUEST,
+        PreviousUserDataException: HTTPStatus.BAD_REQUEST,
+        PasswordStrengthException: HTTPStatus.BAD_REQUEST,
+        UserNotExistsException: HTTPStatus.NOT_FOUND
+    })
     def update_password(self):
-        try:
-            Validator.validate_required_fields(data=request.json, required_fields=["password"])
-            payload_data = request.get_json()
-            return jsonify(UserService.update_password(identity=get_jwt_identity(),
-                                                       new_password=payload_data.get("password"))), HTTPStatus.OK
-
-        except MissingRequiredFieldsException as e:
-            return jsonify({"error": str(e)}), HTTPStatus.BAD_REQUEST
-
-        except PreviousUserDataException as e:
-            return jsonify({"error": str(e)}), HTTPStatus.BAD_REQUEST
-
-        except PasswordStrengthException as e:
-            return jsonify({"error": str(e)}), HTTPStatus.BAD_REQUEST
-
-        except UserNotExistsException as e:
-            return jsonify({"error": str(e)}), HTTPStatus.NOT_FOUND
-
-        except Exception as e:
-            return jsonify({"error": str(e)}), HTTPStatus.INTERNAL_SERVER_ERROR
+        Validator.validate_required_fields(data=request.json, required_fields=["password"])
+        payload_data = request.get_json()
+        return jsonify(UserService.update_password(identity=get_jwt_identity(),
+                                                   new_password=payload_data.get("password"))), HTTPStatus.OK
 
     @jwt_required()
+    @ExceptionsHandler.handle_exceptions({
+        MissingRequiredFieldsException: HTTPStatus.BAD_REQUEST,
+        UserAlreadyExistsException: HTTPStatus.CONFLICT,
+        EmailNotValidError: HTTPStatus.BAD_REQUEST,
+        UserNotExistsException: HTTPStatus.NOT_FOUND,
+        PreviousUserDataException: HTTPStatus.BAD_REQUEST
+    })
     def update_email(self):
-        try:
-            Validator.validate_required_fields(data=request.json, required_fields=["email"])
-            payload_data = request.get_json()
-            return jsonify(UserService.update_email(identity=get_jwt_identity(),
-                                                    new_email=payload_data.get("email")))
-
-        except MissingRequiredFieldsException as e:
-            return jsonify({"error": str(e)}), HTTPStatus.BAD_REQUEST
-
-        except UserAlreadyExistsException as e:
-            return jsonify({"error": str(e)}), HTTPStatus.CONFLICT
-
-        except EmailNotValidError as e:
-            return jsonify({"error": str(e)}), HTTPStatus.BAD_REQUEST
-
-        except UserNotExistsException as e:
-            return jsonify({"error": str(e)}), HTTPStatus.NOT_FOUND
-
-        except PreviousUserDataException as e:
-            return jsonify({"error": str(e)}), HTTPStatus.BAD_REQUEST
-
-        except Exception as e:
-            return jsonify({"error": str(e)}), HTTPStatus.INTERNAL_SERVER_ERROR
+        Validator.validate_required_fields(data=request.json, required_fields=["email"])
+        payload_data = request.get_json()
+        return jsonify(UserService.update_email(identity=get_jwt_identity(),
+                                                new_email=payload_data.get("email")))
 
     @jwt_required()
+    @ExceptionsHandler.handle_exceptions({
+        MissingRequiredFieldsException: HTTPStatus.BAD_REQUEST,
+        UserNotExistsException: HTTPStatus.NOT_FOUND,
+        UsernameNotValidException: HTTPStatus.BAD_REQUEST,
+        PreviousUserDataException: HTTPStatus.BAD_REQUEST
+    })
     def update_username(self):
-        try:
-            Validator.validate_required_fields(data=request.json, required_fields=["username"])
-            payload_data = request.get_json()
-            return jsonify(UserService().update_username(identity=get_jwt_identity(),
-                                                         new_username=payload_data.get("username"))), HTTPStatus.OK
-
-        except MissingRequiredFieldsException as e:
-            return jsonify({"error": str(e)}), HTTPStatus.BAD_REQUEST
-
-        except UserNotExistsException as e:
-            return jsonify({"error": str(e)}), HTTPStatus.NOT_FOUND
-
-        except UsernameNotValidException as e:
-            return jsonify({"error": str(e)}), HTTPStatus.BAD_REQUEST
-
+        Validator.validate_required_fields(data=request.json, required_fields=["username"])
+        payload_data = request.get_json()
+        return jsonify(UserService().update_username(identity=get_jwt_identity(),
+                                                     new_username=payload_data.get("username"))), HTTPStatus.OK
         # TODO - make a decision, might be deleted
         # except OffensiveUsernameException as e:
         #     return jsonify({"error": str(e)}), HTTPStatus.BAD_REQUEST
-
-        except PreviousUserDataException as e:
-            return jsonify({"error": str(e)}), HTTPStatus.BAD_REQUEST
-
-        except Exception as e:
-            return jsonify({"error": str(e)}), HTTPStatus.INTERNAL_SERVER_ERROR
 
     def as_blueprint(self):
         return self.blueprint
